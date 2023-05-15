9f9c86f3b6a5e224fecc0545ff64a9fd4a5e3c4a
Nitin Vavdiya <nitin.vavdiya@smartsensesolutions.com>
2023-05-15 17:03:48 +0530

    Init commit to start did:web development

---
+++ src/main/java/org/eclipse/tractusx/managedidentitywallets/ManagedIdentityWalletsApplication.java
@@ -0,0 +1,13 @@
+package org.eclipse.tractusx.managedidentitywallets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagedIdentityWalletsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManagedIdentityWalletsApplication.class, args);
+	}
+
+}
