fd060105f1841d5b92041fc0bfcefe25251da199
Nitin Vavdiya <nitin.vavdiya@smartsensesolutions.com>
2023-05-17 18:55:02 +0530

    Create wallet API skeleton for review ref: https://github.com/eclipse-tractusx/managed-identity-wallet/issues/1

---
+++ src/main/java/org/eclipse/tractusx/managedidentitywallets/service/WalletService.java
@@ -0,0 +1,69 @@
+/*
+ * *******************************************************************************
+ *  Copyright (c) 2021,2023 Contributors to the Eclipse Foundation
+ *
+ *  See the NOTICE file(s) distributed with this work for additional
+ *  information regarding copyright ownership.
+ *
+ *  This program and the accompanying materials are made available under the
+ *  terms of the Apache License, Version 2.0 which is available at
+ *  https://www.apache.org/licenses/LICENSE-2.0.
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ *  WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ *  License for the specific language governing permissions and limitations
+ *  under the License.
+ *
+ *  SPDX-License-Identifier: Apache-2.0
+ * ******************************************************************************
+ */
+
+package org.eclipse.tractusx.managedidentitywallets.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.eclipse.tractusx.managedidentitywallets.dao.entity.Wallet;
+import org.eclipse.tractusx.managedidentitywallets.dao.repository.WalletRepository;
+import org.eclipse.tractusx.managedidentitywallets.dto.CreateWalletRequest;
+import org.springframework.stereotype.Service;
+
+/**
+ * The type Wallet service.
+ */
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class WalletService {
+
+    private final WalletRepository walletRepository;
+
+
+    /**
+     * Create wallet.
+     *
+     * @param request the request
+     * @return the wallet
+     */
+    public Wallet createWallet(CreateWalletRequest request){
+        validateCreateWallet(request);
+
+        //create private key pair
+
+        //create did json
+
+        //create wallet amd save key
+
+        log.debug("Wallet created for bpn ->{}", request.getBpn());
+        return new Wallet();
+    }
+
+    private void validateCreateWallet(CreateWalletRequest request){
+        //TODO validations
+
+        //check duplicate BPN
+
+        //check if wallet is already created
+
+    }
+}
