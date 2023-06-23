20c8d7a7230a761ab12528ef38f53c956388abfa
Dominik Pinsel <dominik.pinsel@mercedes-benz.com>
2023-06-23 09:43:46 +0200

    remove test profile
    Signed-off-by: Dominik Pinsel <dominik.pinsel@mercedes-benz.com>

--- src/test/java/org/eclipse/tractusx/managedidentitywallets/vp/PresentationTest.java
+++ src/test/java/org/eclipse/tractusx/managedidentitywallets/vp/PresentationTest.java
@@ -51,7 +51,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.*;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 
 import java.text.ParseException;
@@ -61,7 +60,6 @@ import java.util.Map;
 import java.util.UUID;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = {ManagedIdentityWalletsApplication.class})
-@ActiveProfiles("test")
 @ContextConfiguration(initializers = {TestContextInitializer.class})
 class PresentationTest {
 
