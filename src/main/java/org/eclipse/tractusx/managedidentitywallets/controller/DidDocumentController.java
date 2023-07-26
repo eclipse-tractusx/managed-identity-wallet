d85cfa9a9dfac4355b47f04844a28eb144eced56
Nitin <45592624+nitin-vavdiya@users.noreply.github.com>
2023-07-26 14:18:16 +0530

    Update src/main/java/org/eclipse/tractusx/managedidentitywallets/controller/DidDocumentController.java
    Co-authored-by: Maximilian Schmidt (ZF Friedrichshafen AG) <104348964+maximilianschmidt-zf@users.noreply.github.com>

--- src/main/java/org/eclipse/tractusx/managedidentitywallets/controller/DidDocumentController.java
+++ src/main/java/org/eclipse/tractusx/managedidentitywallets/controller/DidDocumentController.java
@@ -93,7 +93,7 @@ public class DidDocumentController {
                                "verificationMethod": [
                                  {
                                    "controller": "did:web:localhost:BPNL000000000000",
-                                   "id": "did:web:localhost:BPNL000000000000#",
+                                   "id": "did:web:localhost:BPNL000000000000",
                                    "publicKeyJwk": {
                                      "crv": "Ed25519",
                                      "kty": "OKP",
