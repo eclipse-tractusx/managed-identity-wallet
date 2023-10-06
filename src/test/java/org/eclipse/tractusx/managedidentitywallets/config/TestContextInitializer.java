82ddbd76b93db4799c42c279d1f5a5a3bb6eb85f
Dominik Pinsel <dominik.pinsel@mercedes-benz.com>
2023-10-06 09:21:35 +0200

    chore: repair tests

--- src/test/java/org/eclipse/tractusx/managedidentitywallets/config/TestContextInitializer.java
+++ src/test/java/org/eclipse/tractusx/managedidentitywallets/config/TestContextInitializer.java
@@ -50,6 +50,7 @@ public class TestContextInitializer implements ApplicationContextInitializer<Con
                 "server.port=" + port,
                 "miw.host: localhost:${server.port}",
                 "miw.enforceHttps=false",
+                "miw.vcExpiryDate=1-1-2030",
                 "miw.encryptionKey="+ Base64.getEncoder().encodeToString(secretKey.getEncoded()),
                 "miw.authorityWalletBpn: BPNL000000000000",
                 "miw.authorityWalletName: Test-X",
