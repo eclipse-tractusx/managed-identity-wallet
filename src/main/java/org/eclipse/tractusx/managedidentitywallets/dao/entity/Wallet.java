579f199ab15b1c2a262b8fcb0a7010a051a3ae1c
Pascal Manaras <manaras@xignsys.com>
2024-03-21 10:44:54 +0100

    remove unecessary imports and vars

--- src/main/java/org/eclipse/tractusx/managedidentitywallets/dao/entity/Wallet.java
+++ src/main/java/org/eclipse/tractusx/managedidentitywallets/dao/entity/Wallet.java
@@ -23,9 +23,20 @@ package org.eclipse.tractusx.managedidentitywallets.dao.entity;
 
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import jakarta.persistence.*;
-import lombok.*;
-import org.eclipse.tractusx.managedidentitywallets.domain.KeyStorageType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Convert;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Transient;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.eclipse.tractusx.managedidentitywallets.domain.SigningServiceType;
 import org.eclipse.tractusx.managedidentitywallets.utils.StringToDidDocumentConverter;
 import org.eclipse.tractusx.ssi.lib.model.did.DidDocument;
