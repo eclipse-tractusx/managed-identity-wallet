b0469ef3744dea19ed1d70c0a495478f66482fc1
Dominik Pinsel <dominik.pinsel@mercedes-benz.com>
2024-05-16 10:46:13 +0200

    update docusaurus repo
    Signed-off-by: Dominik Pinsel <dominik.pinsel@mercedes-benz.com>

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -11,11 +11,11 @@ const config: Config = {
     url: 'https://github.com',
     // Set the /<baseUrl>/ pathname under which your site is served
     // For GitHub pages deployment, it is often '/<projectName>/'
-    baseUrl: '/eclipse-tractusx/managed-identity-wallet/',
+    baseUrl: '/managed-identity-wallet/',
 
     // GitHub pages deployment config.
     // If you aren't using GitHub pages, you don't need these.
-    organizationName: 'eclipse-tractusx', // Usually your GitHub org/user name.
+    organizationName: 'eDominikPinsel', // Usually your GitHub org/user name.
     projectName: 'managed-identity-wallet', // Usually your repo name.
 
     onBrokenLinks: 'throw',
